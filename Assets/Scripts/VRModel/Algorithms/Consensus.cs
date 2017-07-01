dc6e6bcc91b049a4a3ff9764f16af34038ba742a
imyjimmy <imyjimmy@gmail.com>
2017-06-30 18:23:12 -0700

    starting MSA and pairwise alignments

--- Assets/Scripts/VRModel/Algorithms/Consensus.cs
+++ Assets/Scripts/VRModel/Algorithms/Consensus.cs
@@ -5,10 +5,6 @@ namespace VRModel.Algorithms {
 
 	public class Consensus {
 
-		public float identity { get; set; }
-		public List<string> agree { get; set; }
-		public List<string> disagree { get; set; }
-
 		public List<string> comparisons { get; private set; }
 
 		public Seq type { get; private set; }
@@ -16,5 +12,7 @@ namespace VRModel.Algorithms {
 		public Consensus() {
 
 		}
+
+		
 	}
 }
